e05bba4ed1456149868d7ea61dc2e91204b43a7e
Oleh Sklyarenko <tyne.oskl@gmail.com>
2013-03-24 16:47:50 +0200

    Reviewing code that reads log file

--- client/powerteam_Ruby_client.rb
+++ client/powerteam_Ruby_client.rb
@@ -1,6 +1,6 @@
 line_num=0
 text=File.open('log.txt').read
-text.gsub!(/\r\n?/, "\n")
+text.gsub!(/\r\n?/, "\n") #review> this replacement won't work - see documentation for gsub! method
 text.each_line do |line|
   print "#{line_num += 1} #{line}"
 end
