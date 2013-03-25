040dc5c72d76b06499111d81cc442fa4a242c6d6
Babichok <ababichok@morningcoffeesoft.com>
2013-03-25 19:52:19 +0200

    Ruby post

--- client/powerteam_Ruby_client.rb
+++ client/powerteam_Ruby_client.rb
@@ -1,11 +1,9 @@
-<<<<<<< HEAD
 require 'net/http'
 
 class Client
 	def print_log
 		line_num=0
 		text=File.open('log.txt').read
-		text.gsub!(/\r\n?/, "\n")
 		text.each_line do |line|
 	  	print "#{line_num += 1} #{line}"
 		end
@@ -15,20 +13,15 @@ class Client
 		uri = URI('http://httpbin.org/post')
 		res = Net::HTTP.post_form(uri, 'q' => 'ruby', 'max' => '50')
 		puts res.body
+		case res
+		when Net::HTTPSuccess, Net::HTTPRedirection
+  			puts "NO ERRORS!"	
+		else
+  			puts "ERROR!"
+		end
 	end
-=======
-line_num=0
-text=File.open('log.txt').read
-text.gsub!(/\r\n?/, "\n") #review> this replacement won't work - see documentation for gsub! method
-text.each_line do |line|
-  print "#{line_num += 1} #{line}"
->>>>>>> 2e8073542809759c9e7ef7d0568ccbbb4cb39602
 end
 
 client = Client.new
 client.print_log
 client.post
-
-
-
-
