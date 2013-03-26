018632bcaf599f3b0ee2537b243c994df1b10a6e
Markiyan Matviyiv <mmatviyiv@morningcoffeesoft.com>
2013-03-27 01:19:46 +0200

    Exception fix

---
+++ server/powerteam-server/src/main/java/org/morningcoffee/powerteam/server/Main.java
@@ -0,0 +1,34 @@
+package org.morningcoffee.powerteam.server;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.ServletException;
+import java.io.IOException;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.Request;
+import org.eclipse.jetty.server.handler.AbstractHandler;
+ 
+public class Main extends AbstractHandler
+{
+    public void handle(String target,
+                       Request baseRequest,
+                       HttpServletRequest request,
+                       HttpServletResponse response) 
+        throws IOException, ServletException
+    {
+        baseRequest.setHandled(true);
+        
+    	String data = request.getParameter("data");
+        System.out.println(data);
+        
+    }
+ 
+    public static void main(String[] args) throws Exception
+    {
+        Server server = new Server(8080);
+        server.setHandler(new Main());
+ 
+        server.start();
+        server.join();
+    }
+}
