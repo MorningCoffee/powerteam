fe4df87b950313aa57cc1af3a56de84a5051b366
Babichok <ababichok@morningcoffeesoft.com>
2013-03-28 19:42:21 +0200

    Check return values

--- client/powerteam_Bash_client.sh
+++ client/powerteam_Bash_client.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-export LOG=$(git reflog show origin/master --date=local --since="yesterday")	#get reflog information
+export LOG=$(git reflog show origin/master --date=local)	#get reflog information
 
 export USER=$(git config user.name)	
 
 ruby powerteam_Ruby_client.rb
 
+echo "Finish shell: " $?
+
+
+#--since="yesterday"
--- client/powerteam_Ruby_client.rb
+++ client/powerteam_Ruby_client.rb
@@ -7,22 +7,24 @@ class Client
 		type='client' 
 		user=ENV['USER']
 		log_r=ENV['LOG']
-		hash=log_r[0..6]
-		date=log_r[36..59]
-	
+		#hash=log_r[0..6]
+		#date=log_r[36..59]
+		
+		puts log_r		
+		
 		log_r.each_line do |line|			
 			if line.include? "update by push"
 				puts line
+				hash=line[0..6]
+				date=line[36..59]			
 				json_string = {
 			      		"type"=>type, 
 			      		"user"=>user,
 					"hash"=>hash,
 					"date"=>date
 			     	}.to_json
-				puts "\n"
 				puts json_string
-				puts "\n"
-
+				
 				uri = URI('http://httpbin.org/post')
 				res = Net::HTTP.post_form(uri, json_string)
 		
@@ -32,6 +34,7 @@ class Client
 				else
   					puts "ERROR!"
 				end
+				puts "\n"
 			end
 		end						
 	end
@@ -40,3 +43,16 @@ end
 
 client = Client.new
 client.post_json_string
+
+at_exit do
+  if $!.nil? || $!.is_a?(SystemExit) && $!.success?
+    puts 'Finish ruby: 0'
+  else
+    code = $!.is_a?(SystemExit) ? $!.status : 1
+    puts "failure with code #{code}"
+  end
+end
+
+
+
+
