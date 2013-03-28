d6c9e1fa3769466894fd2ffb8310cffd419a42e8
Babichok <ababichok@morningcoffeesoft.com>
2013-03-28 16:26:15 +0200

    Create JSON string and post

--- client/powerteam_Ruby_client.rb
+++ client/powerteam_Ruby_client.rb
@@ -2,57 +2,41 @@ require 'net/http'
 require 'json'
 
 class Client
-	def print_log
-		print ENV['LOG'] 
-	end
 
-	def post
-		uri = URI('http://httpbin.org/post')
-		res = Net::HTTP.post_form(uri, 'q' => 'ruby', 'max' => '50')
-		#puts res.body
-		
-		case res
-		when Net::HTTPSuccess, Net::HTTPRedirection
-  			puts "NO ERRORS!"
-			data='111'
-			puts data
-		else
-  			puts "ERROR!"
-		end
-	end
-
-
-	def json_string
-		puts "\n\nJSON\n\n"
-		log_r=ENV['LOG']
+	def post_json_string		
+		type='client' 
 		user=ENV['USER']
-		type='client' 	
-		
-		hash=log_r[0..7]
+		log_r=ENV['LOG']
+		hash=log_r[0..6]
 		date=log_r[36..59]
+	
+		log_r.each_line do |line|			
+			if line.include? "update by push"
+				puts line
+				json_string = {
+			      		"type"=>type, 
+			      		"user"=>user,
+					"hash"=>hash,
+					"date"=>date
+			     	}.to_json
+				puts "\n"
+				puts json_string
+				puts "\n"
 
-		puts type
-		puts user
-		puts hash		
-		puts date
-
-		puts "\n"
+				uri = URI('http://httpbin.org/post')
+				res = Net::HTTP.post_form(uri, json_string)
 		
-
-		#log_r.scan("\z")
-		
-		json_string = {
-			      	"type"=>type, 
-			      	"user"=>user,
-				"hash"=>hash,
-				"date"=>date,
-			      }.to_json
-		puts json_string						
-		
+				case res
+				when Net::HTTPSuccess, Net::HTTPRedirection
+  					puts "NO ERRORS!"
+				else
+  					puts "ERROR!"
+				end
+			end
+		end						
 	end
+
 end
 
 client = Client.new
-client.print_log
-#client.post
-client.json_string
+client.post_json_string
