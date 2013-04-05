982dd701024bc3508c1b668efdab4ccfcc5c401c
Babichok <ababichok@morningcoffeesoft.com>
2013-04-05 16:18:14 +0300

    Change initialize for errors

--- client/uninstall.sh
+++ client/uninstall.sh
@@ -3,12 +3,29 @@
 cd /etc/cron.d/
 
 sudo rm -f powerteam
-echo "powerteam - REMOVED"
 
-sudo rm -f powerteam_Bash_client.sh
-echo "powerteam_Bash_client.sh - REMOVED"
+if [ $? -eq 0 ]
+then
+	echo "powerteam - REMOVED"
+else
+	echo "ERROR: CAN'T DELETE FILE POWERTEAM"
+	exit 1
+fi
 
-sudo rm -f powerteam_Ruby_client.rb
-echo "powerteam_Ruby_client.rb - REMOVED"
+if [ $? -eq 0 ]
+then
+	echo "powerteam_Bash_client.sh - REMOVED"
+else
+	echo "ERROR: CAN'T DELETE FILE POWERTEAM_BASH_CLIENT.SH"
+	exit 1
+fi
+
+if [ $? -eq 0 ]
+then
+	echo "powerteam_Ruby_client.rb - REMOVED"
+else
+	echo "ERROR: CAN'T DELETE FILE POWERTEAM_RUBY_CLIENT.RB"
+	exit 1
+fi
 
 echo "UNINSTALL - SUCCESS"
